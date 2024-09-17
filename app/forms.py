18294459843b99bf29e4e9e9fe9023f35d118088
Geff115 <gabrielnoah129@gmail.com>
2024-09-17 18:57:34 +0100

    Update: Modified the login form for a field

--- app/forms.py
+++ app/forms.py
@@ -52,7 +52,7 @@ class LoginForm(FlaskForm):
     """
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('Sign in')
 
 class ResetPasswordForm(FlaskForm):
     """
